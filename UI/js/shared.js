b90f59dfc2a9a2f287f97dc9f41795e9ab5e4913
Pascal Ulor <pascalulor@yahoo.com>
2018-05-17 10:54:37 +0100

    Implement feedback on form and nav bar

--- UI/js/shared.js
+++ UI/js/shared.js
@@ -15,3 +15,13 @@ toggleButton.addEventListener('click', () => {
     mobileNav.classList.add('open');
     backdrop.classList.add('open');
 });
+
+backdrop.removeEventListener('click', () => {
+    mobileNav.classList.remove('open');
+    backDrop();
+});
+
+toggleButton.removeEventListener('click', () => {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
