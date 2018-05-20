83b394580eb6a463155c0abac6a00ad8555f7b2c
Pascal Ulor <pascalulor@yahoo.com>
2018-05-20 15:35:17 +0100

    [#157643970] create and test API for userlogin

--- server/models/testData.js
+++ server/models/testData.js
@@ -1,29 +1,35 @@
 const userDb = [
-    {
-        id: 1,
-        fullName: 'Don Pascal',
-        email: 'pc@yahoo.com',
-        password: '123'
-    },
-    {
-        id: 2,
-        fullName: 'Clark Kent',
-        email: 'clark@gmail.com',
-        password: '453'
-    }
+  {
+    id: 1,
+    fullName: 'Don Pascal',
+    email: 'pc@yahoo.com',
+    password: '123'
+  },
+  {
+    id: 2,
+    fullName: 'Clark Kent',
+    email: 'clark@gmail.com',
+    password: '453'
+  },
+  {
+    id: 3,
+    fullName: 'Mike',
+    email: 'mk@yahoo.com',
+    password: '123'
+  }
 ];
 
 const requestDb = [
-    {
-        id: 1,
-        location: 'Lagos',
-        Details: 'Air conditioner is faulty'
-    },
-    {
-        id: 2,
-        location: 'Jos',
-        Details: 'Systems are bad'
-    }
+  {
+    id: 1,
+    location: 'Lagos',
+    Details: 'Air conditioner is faulty'
+  },
+  {
+    id: 2,
+    location: 'Jos',
+    Details: 'Systems are bad'
+  }
 ];
 
 
