b334e3c4fb243849525a8e0cc8ccfc02c7aa53c5
Pascal Ulor <pascalulor@yahoo.com>
2018-05-21 09:58:30 +0100

    [#157724752] install chokidar for hot reload and delete nodemon from package.json script

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import chokidar from 'chokidar';
 
 
 import routes from './server/routes/routes';
 
 const app = express();
+
+const watcher = chokidar.watch('server');
 
 const port = process.env.PORT || 3000;
 
@@ -13,14 +16,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
+// Watcher function
+watcher.on('ready', () => {
+  watcher.on('all', () => {
+    console.log('Clearing /dist/ module cache from server');
+    Object.keys(require.cache).forEach((id) => {
+      if (/[/\\]app[/\\]/.test(id)) delete require.cache[id];
+    });
+  });
+});
 
 // Home page route
 app.get('/', (req, res) => {
-    res.status(200);
-    res.json({
-        name: 'Welcome to Maintenance Tracker',
-        message: 'Your Service, Our Pleasure'
-    });
+  res.status(200);
+  res.json({
+    name: 'Welcome to Maintenance Tracker',
+    message: 'Your Service, Our Pleasure'
+  });
 });
 
 
@@ -29,9 +41,9 @@ app.use('/api/v1/', routes);
 
 // Trivial Route
 app.get('*', (req, res) => {
-    res.status(404).json({
-        message: 'Invalid routes'
-    });
+  res.status(404).json({
+    message: 'Invalid routes'
+  });
 });
 
 
