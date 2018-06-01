0f9fb2cd2c5475afae61cb71ac6b6c09b35fc7da
Pascal Ulor <pascalulor@yahoo.com>
2018-06-01 11:40:51 +0100

    bug(improve-validation): Improve on validations [Fixes #158020860]

--- server/helpers/checkInput.js
+++ server/helpers/checkInput.js
@@ -4,7 +4,7 @@ import validator from 'validator';
 const checkItem = (inputValue) => {
   const errors = {};
   Object.keys(inputValue).forEach((key) => {
-    if (!inputValue[key]) {
+    if (!inputValue[key] || (validator.isEmpty(inputValue[key]))) {
       errors[key] = `${key} field is undefined`;
     } else {
       /*
@@ -16,34 +16,26 @@ const checkItem = (inputValue) => {
         }
       }
       if (key === 'details') {
-        if (!(validator.isEmpty(inputValue[key]))) {
-          if (!(validator.isLength(inputValue[key], { min: 20, max: 1000 }))) {
-            errors[key] = `${key} field must be between 20 to 1000 characters`;
-          }
+        if (!(validator.isLength(inputValue[key], { min: 20, max: 1000 }))) {
+          errors[key] = `${key} field must be between 20 to 1000 characters`;
         }
       }
       /*
       *User Validation
       */
       if (key === 'firstName' || key === 'lastName' || key === 'title') {
-        if (!(validator.isEmpty(inputValue[key]))) {
-          if (inputValue[key].search(/[^A-Za-z\s]/) !== -1) {
-            errors[key] = `${key} can only be alphabetical`;
-          }
+        if (inputValue[key].search(/[^A-Za-z\s]/) !== -1) {
+          errors[key] = `${key} can only be alphabetical`;
         }
       }
       if (key === 'email') {
-        if (!(validator.isEmpty(inputValue[key]))) {
-          if (!validator.isEmail(inputValue[key])) {
-            errors[key] = `Invalid ${key}`;
-          }
+        if (!validator.isEmail(inputValue[key])) {
+          errors[key] = `Invalid ${key}`;
         }
       }
       if (key === 'password') {
-        if (!(validator.isEmpty(inputValue[key]))) {
-          if (!validator.isLength(inputValue[key], { min: 8, max: 50 })) {
-            errors[key] = `${key} must between 8 and 50 characters`;
-          }
+        if (!validator.isLength(inputValue[key], { min: 8, max: 50 })) {
+          errors[key] = `${key} must between 8 and 50 characters`;
         }
       }
     }
